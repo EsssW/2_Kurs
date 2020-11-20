ffc136894b309261b58af39a63a4333c43e9d1e2
EsssW <43808999+EsssW@users.noreply.github.com>
2020-11-20 14:39:13 +0500

    Add files via upload

---
+++ Polinom.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Para_Stack
+{
+    class Element 
+    {
+        public int power { get; set; }
+        public double kf { get; set; }
+        public Element(double _kf,int _power)
+        {
+            power = _power;
+            kf = _kf;
+        }
+        public Element() { }
+        
+    }
+    
+    class Polinom 
+    {
+        public List<Element> list = new List<Element>();
+        public void makelist()
+        {
+            Element temp = new Element();
+            Console.WriteLine("Введите полином по возрастанию степени | Для выхода ввыедите -1|");
+            int power = 0, kf = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите степень");
+                power = int.Parse(Console.ReadLine());
+                if (power != -1)
+                {
+                    Console.WriteLine($"Введите коэфициент при X^{power}");
+                    kf= int.Parse(Console.ReadLine());
+                    temp.kf = kf;
+                    temp.power = power;
+                    list.Add(temp);
+                }
+                else return;
+            }
+        }
+        public  void Add(Element temp )
+        {
+            list.Add(temp); 
+        }
+        public void Add(double kf,int power)
+        {
+            list.Add(new Element(kf,power));
+        }
+        public void Print()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i < list.Count - 1)
+                {
+                    if (list[i].power == 0)
+                        Console.Write($"{list[i].kf} + ");
+                    else
+                        if (list[i].kf == 0) i++;
+                    else Console.Write($"{list[i].kf}X^{list[i].power} + ");
+                }
+                else
+                {
+                    if (list[i].power == 0) Console.Write($"{list[i].kf} + ");
+                    else Console.Write($"{list[i].kf}X^{list[i].power}");
+                }
+            }
+        }
+        public static Polinom operator +(Polinom p1, Polinom p2)
+        {
+            //выбираем полином наибольшей и наименьшей степени
+            Polinom maxPowerPoly = p1.list.Count <= p2.list.Count ? p2 : p1;
+            Polinom minPowerPoly = p1.list.Count < p2.list.Count ? p1 : p2;
+
+            //создаем массив для предварительного результата
+            Polinom Result = new Polinom();
+
+            Result.list=new List<Element>(maxPowerPoly.list.Count );
+
+            //складываем члены начиная с нулевой до максимальной степени для полинома меньшей степени
+            for (int i = 0; i < minPowerPoly.list.Count ; i++)
+            {
+                Result.Add(SumElement(minPowerPoly.list[i], maxPowerPoly.list[i]));
+            }
+            //дописываем старшие члены от полинома большей степени
+            for (int i = minPowerPoly.list.Count; i < maxPowerPoly.list.Count ; i++)
+            {
+                Result.Add(maxPowerPoly.list[i]);
+            }
+            return Result;
+        }
+        // вспомогательный мето для сложения полиномов
+        private static Element SumElement(Element el1, Element el2)
+            => new Element(el1.kf + el2.kf, el1.power);
+
+        public static Polinom operator *(Polinom p1, double digit)
+        {
+            Polinom npolinom = new Polinom();
+            for (int i = 0; i < p1.list.Count; i++)
+            {
+                npolinom.Add(p1.list[i].kf * digit, p1.list[i].power);
+            }
+            return npolinom;
+        }
+        public static Polinom operator *(Polinom p1, Polinom p2)
+        {
+            //выбираем полином наибольшей и наименьшей степени
+            Polinom maxPowerPoly = p1.list.Count <= p2.list.Count ? p2 : p1;
+            Polinom minPowerPoly = p1.list.Count < p2.list.Count ? p1 : p2;
+
+            //создаем массив для предварительного результата
+            Polinom Result = new Polinom();
+
+            Result.list = new List<Element>(maxPowerPoly.list.Count);
+
+            //складываем члены начиная с нулевой до максимальной степени для полинома меньшей степени
+            for (int i = 0; i < minPowerPoly.list.Count; i++)
+            {
+                Result.Add(MultElement(minPowerPoly.list[i], maxPowerPoly.list[i]));
+            }
+            //дописываем старшие члены от полинома большей степени
+            for (int i = minPowerPoly.list.Count; i < maxPowerPoly.list.Count; i++)
+            {
+                Result.Add(maxPowerPoly.list[i]);
+            }
+            return Result;
+        }
+        // вспомогаельный метод для умножения полиномов
+        private static Element MultElement(Element el1, Element el2)
+            => new Element(el1.kf * el2.kf, el1.power);
+        // Вычисление многочлена в определенной точке
+        public double ResultInPoint(int point)
+        {
+            double sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i].kf * Math.Pow(point, Convert.ToDouble(list[i].power));
+            }
+            return sum;
+        }
+        // Интегрирование полинома 
+        public Polinom Integral()
+        {
+            Polinom integral = new Polinom();
+            for (int i = 0; i < list.Count; i++)
+            {
+                integral.Add(Integral(list[i]));
+            }
+            return integral;
+        }
+        // Вспомогательный метод для интегрирования полинома
+        private Element Integral(Element el)
+            => new Element(Convert.ToDouble(el.kf / (el.power + 1)), el.power + 1);
+
+        // Диффференцирование полинома
+        public Polinom Differential()
+        {
+            Polinom differential = new Polinom();
+            for (int i = 0; i < list.Count; i++)
+            {
+                differential.Add(Differential(list[i]));
+            }
+            return differential;
+        }
+        // Вспомогательный метод для диффиринцирвоания
+        private Element Differential(Element el)
+           => new Element(Convert.ToDouble(el.kf * el.power), el.power -1);
+
+        
+
+    }
+}
---
+++ SortedClass.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Para_Stack
+{
+    class SortEL<Type> where Type : IComparable
+    {
+        Type val;
+        SortEL<Type> next, prev;
+        public SortEL(Type v)
+        { val = v; next = null; prev = null; }
+        public Type Value
+        {
+            get => val;
+            set { val = value; }
+        }
+        public SortEL<Type> Next
+        {
+            get => next;
+            set { next = value; }
+        }
+        public SortEL<Type> Prev
+        {
+            get => prev;
+            set { prev = value; }
+        }
+    }
+    class SortedClass<Type> where Type : IComparable
+    {
+        public SortEL<Type> first;
+        int count = 0;
+
+
+
+
+        public SortedClass() { first = null; }
+        public void Add(Type k)
+        {
+            SortEL<Type> tmp;
+            if (first == null) { first = new SortEL<Type>(k); count++; return; }
+            if (first.Value.CompareTo(k) > 0)
+            {
+                tmp = new SortEL<Type>(k); count++;
+                tmp.Next = first; first.Prev = tmp; first = tmp; return;
+            }
+            SortEL<Type> p1, p2;
+            for (p1 = first, p2 = first; p2 != null && p2.Value.CompareTo(k) < 0; p1 = p2, p2 = p2.Next) ;
+            tmp = new SortEL<Type>(k);
+            tmp.Next = p2;
+            tmp.Prev = p1;
+            if (p2 != null)
+                p2.Prev = tmp;
+            p1.Next = tmp;
+            count++;
+        }
+        public void Remove(Type k)
+        {
+            if (first == null) return;
+            if (k.CompareTo(first.Value) == 0)
+            {
+                first = first.Next;
+                first.Prev = null;
+                count--; return;
+            }
+            SortEL<Type> t1 = first, t2 = first;
+            for (; t1 != null && t1.Value.CompareTo(k) != 0; t2 = t1, t1 = t1.Next)
+                if (t1 == null) return;
+            t2.Next = t1.Next;
+            if(t1.Next!=null)
+            t1.Next.Prev = t2;
+            count--;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > count)
+                throw new Exception("index out");
+            if (index == 0)
+            {
+                first = first.Next;
+                first.Prev = null;
+                count--; return;
+            }
+            SortEL<Type> removeEl = first, prevEl = first;
+            for (int j = 0; j <= index; j++)
+            {
+                prevEl= removeEl;
+                removeEl = removeEl.Next;
+            }
+            prevEl.Next = removeEl.Next;
+            if (removeEl.Next != null)
+                removeEl.Next.Prev = prevEl;
+            count--; return;
+        }
+        public int IndexOf(Type k)
+        {
+            SortEL<Type> temp = first;
+            int index = -1;
+            for (int i = 0; temp != null;temp=temp.Next, i++)
+            {
+                if (temp.Value.CompareTo(k) == 0) { index = i; return index; }
+            }
+            return index;
+        }
+        public void Clear()
+        {
+            first = null;
+        }
+        public Type GetMax()
+        {
+            SortEL<Type> temp = first;
+            Type MAX = first.Value;
+            for (; ;temp=temp.Next)
+            {
+                if (temp == null) { MAX = temp.Value; return MAX; }
+            }
+        }
+        public Type GetMin()
+        {
+            if (first == null)
+                return default(Type);
+             return first.Value;
+        }
+
+        public static SortedClass<Type> operator +(SortedClass<Type> l1, SortedClass<Type> l2)
+        {
+            SortedClass<Type> Result = new SortedClass<Type>();
+            SortEL<Type> temp1 = l1.first;
+            SortEL<Type> temp2 = l2.first;
+            for (; temp1 != null; Result.Add(temp1.Value), temp1 = temp1.Next) ;
+            for (; temp2 != null; Result.Add(temp2.Value), temp2 = temp2.Next) ;
+
+            return Result;
+        }
+        public int Count { get => count; }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            for (SortEL<Type> p = first; p != null; p = p.Next)
+                Console.Write(p.Value + " -> ");
+            Console.WriteLine();
+        }
+    }
+}
